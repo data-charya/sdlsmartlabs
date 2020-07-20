f72f7a226ea291d8414cd92cdf4b537dfaeaff52
Vignesh Shetty <vigneshshettyalike@gmail.com>
2020-07-20 19:42:31 +0530

    dbms

--- sql/database.sql
+++ sql/database.sql
@@ -3,23 +3,21 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `admins` (
   `username` varchar(35) NOT NULL PRIMARY KEY,
+  `First Name` varchar(25) NOT NULL,
+  `Last Name` varchar(25) NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `Phone No` varchar(12) NOT NULL,
   `password` varchar(60) NOT NULL
   ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `blog` (
-  `BlogID` int NOT NULL PRIMARY KEY,
-  `Title` varchar(50) NOT NULL,
+  `BlogID` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `Title` varchar(100) NOT NULL,
   `Author` varchar(100) NOT NULL,
   `image` varchar(100) NOT NULL,
+  `Content` text NOT NULL,
   `Date` Date NOT NULL,
   `Status` int NOT NULL
-  ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-CREATE TABLE `blogblock` (
-  `ParaID` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `BlogID` int NOT NULL,
-  `Image` varchar(100) NOT NULL,
-  `Content` text NOT NULL
   ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `messages` (
@@ -32,7 +30,7 @@ CREATE TABLE `messages` (
   `Status` int NOT NULL
   ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `admins` VALUES('admin','$2y$10$/nqSUUZscwB5CNCYWug7Zu24UMKDUgALkYYyDqeQUf1CQW/IGpJGu');
+INSERT INTO `admins` VALUES('admin','Administrator','Inces','admin@admin.admin','0000000000','$2y$10$/nqSUUZscwB5CNCYWug7Zu24UMKDUgALkYYyDqeQUf1CQW/IGpJGu');
 
 
 
